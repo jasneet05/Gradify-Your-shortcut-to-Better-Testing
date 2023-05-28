bb86259f4f590b7fa75b27a30962c6b4364165f1
jasneet-arora27 <120542196+jasneet-arora27@users.noreply.github.com>
2023-05-28 21:45:59 +0530

    Add files via upload

---
+++ Gradify- Your shortcut to Better Testing/js/index.js
@@ -0,0 +1,22 @@
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-in');
+    document.querySelector('.all').classList.add('fade-gone');
+    console.log("hello");
+}
+
+function loader2(){
+    document.querySelector('.loader-container').classList.add('fade-gone');
+    document.querySelector('.all').classList.remove('fade-gone');
+    console.log("hello1");
+}
+
+
+
+
+function fadeOut(){
+    setTimeout(loader,0);
+   
+    setTimeout(loader2, 3000);
+}   
+
+window.onload = fadeOut();
